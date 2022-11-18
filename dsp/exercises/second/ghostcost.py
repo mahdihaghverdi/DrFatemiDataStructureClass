32f002bc76decdd56b749984161e7ddc16d95425
Mahdi Haghverdi <mahdihaghverdiliewpl@gmail.com>
2022-11-18 21:53:24 +0330

    Make ghostcost.py Python3.8 compatible

--- dsp/exercises/second/ghostcost.py
+++ dsp/exercises/second/ghostcost.py
@@ -1,29 +1,29 @@
 from itertools import combinations
 from sys import stdin
-from typing import Sequence
+from typing import List, Sequence
 
 
-def subseqs(sequence: Sequence) -> list[list[int]]:
+def subseqs(sequence: Sequence) -> List[List[int]]:
     def key(subseq):
         return len(subseq)
 
     to_ret = [[num] for num in sequence]
     to_ret.extend(
         [
-            sequence[start : end + 1]  # type: ignore
+            sequence[start: end + 1]  # type: ignore
             for start, end in (combinations(range(len(sequence)), 2))
         ],
     )
     return sorted(to_ret, key=key)
 
 
-def max_sum(sequence: list[int]) -> int:
+def max_sum(sequence: List[int]) -> int:
     return max(sum(subseq) for subseq in subseqs(sequence))
 
 
 def do():
     _ = int(input())
-    all_ghosts: list[list[int]] = []
+    all_ghosts: List[List[int]] = []
 
     for idx, line in enumerate(stdin):
         if (-1) ** idx == 1:
