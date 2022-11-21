2e7683a1eef688287401497319dda5832b6bad37
Mahdi Haghverdi <mahdihaghverdiliewpl@gmail.com>
2022-11-21 13:31:00 +0330

    implement `front` and `rear` methods and test them

--- dsp/exercises/stacks_queues/going_round.py
+++ dsp/exercises/stacks_queues/going_round.py
@@ -1,14 +1,36 @@
 from collections import deque
-from typing import Any, Iterable, Optional
+from typing import Any, Generic, Iterable, Optional, TypeVar, Union
+
+T = TypeVar("T")
 
 
-class MyDeque:
+#  deque -> (entrance [appendleft]) [1, 2, 3, 4, 5] (exit [pop])
+class MyDeque(Generic[T]):
     def __init__(self, maxlen: int, /, iterable: Optional[Iterable] = None):
         self.maxlen: int = maxlen
         paras: dict[str, Any] = {"maxlen": maxlen}
         if iterable is not None:
             paras["iterable"] = iterable
-        self._deque: "deque" = deque(**paras)
+        self._deque: "deque[T]" = deque(**paras)
+
+    def front(self) -> Union[T, int]:
+        try:
+            got = self._deque.pop()
+            self._deque.append(got)
+            return got
+        except IndexError:
+            return -1
+
+    def rear(self) -> Union[T, int]:
+        try:
+            got = self._deque.popleft()
+            self._deque.appendleft(got)
+            return got
+        except IndexError:
+            return -1
+
+    def enqueue(self, value: "T"):
+        self._deque.appendleft(value)
 
     def __repr__(self):
         return "MyDeque" + self._deque.__repr__()[5:]
--- tests/exercises/stacks_queues/test_mydeque.py
+++ tests/exercises/stacks_queues/test_mydeque.py
@@ -7,3 +7,25 @@ def test__init__():
     _ = MyDeque(10)
     assert _.maxlen == 10
     assert _._deque == deque(maxlen=10)
+
+
+def test_front_rear_minus_1():
+    _ = MyDeque(10)
+    assert _.front() == -1
+    assert _.rear() == -1
+
+
+def test_front():
+    _ = MyDeque(10, iterable=[1])
+    assert _.front() == 1
+
+    _ = MyDeque(10, iterable=range(5))
+    assert _.front() == 4
+
+
+def test_rear():
+    _ = MyDeque(10, iterable=[1])
+    assert _.rear() == 1
+
+    _ = MyDeque(10, iterable=range(5))
+    assert _.rear() == 0
